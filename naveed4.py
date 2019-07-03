30027d0a899b85ae33ae9e66bee586b3a50c33dd
shaik naveed ahamed <44160062+naveed7860@users.noreply.github.com>
2019-07-03 11:23:00 +0530

    Update and rename naveed4.py to naveed5.py

--- naveed4.py
+++
@@ -1,9 +0,0 @@
-vowel=["a","e","i","o","u"]
-consonant=("b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z")
-a=str(input())
-if a in vowel:
-  print("vowel")
-elif a in consonant:
-      print("consonant")
-else:
-        print("Invalid")
